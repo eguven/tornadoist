49b030fc3622b3c35b3429e8240651267a9418de
Eren Güven <erenguven0@gmail.com>
2013-06-09 14:31:51 +0300

    added MANIFEST.in and updated setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
 import os
 from setuptools import setup
 
-# Utility function to read the README file.
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(
     name = "tornadoist",
-    version = "0.5",
+    version = "0.5.2",
     author = "Eren G\xc3\xbcven",
     author_email = "erenguven0@gmail.com",
     description = "mixins for tornado",
     license = "Apache License, Version 2",
     keywords = ["tornado", "celery"],
     url = "https://github.com/eguven/tornadoist.git",
-    packages=['tornadoist',],
-    long_description=read('README.rst'),
-    classifiers=[
+    packages = ['tornadoist',],
+    long_description = open("README.rst").read(),
+    classifiers = [
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
